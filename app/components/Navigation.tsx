927eccb7db22aac99fb16a0aef4763310219dfb3
CasperPilgaard <casper.pilgaard@hotmail.com>
2026-02-27 02:21:36 +0100

    Added more buttons to navigation

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -1,7 +1,7 @@
-"use client";
+"use client"
 
-import { useState } from "react";
 import Button from "./Button";
+import Image from "next/image";
 
 function scrollToSection(
     id: string,
@@ -24,11 +24,27 @@ export default function Navigation() {
     return (
         <header className="sticky top-0 z-50 h-16">
             <nav className="backdrop-blur-sm bg-black/10 flex justify-between items-center px-6 py-4">
-                <div className="flex gap-6">
+                <Button
+                    onClick={() => window.scrollTo({ top: 0, behavior: "smooth"})}>
+                <Image src="/favicon.svg" alt="CP logo" width={32} height={32} />
+                </Button>
+                <div className="flex gap-6 ml-auto">
                     <Button
                         onClick={() => scrollToSection('about')}
                         className=""
                         >About
+                    </Button>
+
+                    <Button
+                        onClick={() => scrollToSection('about')}
+                        className=""
+                        >Projects
+                    </Button>
+
+                    <Button
+                        onClick={() => scrollToSection('about')}
+                        className=""
+                        >Contact
                     </Button>
 
                 </div>
