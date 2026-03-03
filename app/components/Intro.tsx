63ac3761bc9db4a6c06bd0a73f0661b1dcf80b2e
CasperPilgaard <casper.pilgaard@hotmail.com>
2026-03-04 00:50:27 +0100

    changed github button on intro page

--- app/components/Intro.tsx
+++ app/components/Intro.tsx
@@ -1,4 +1,5 @@
 import Button from "./Button";
+import { FaGithub } from "react-icons/fa"
 
 export default function Intro() {
     return (
@@ -6,12 +7,20 @@ export default function Intro() {
             <p className="text-c-subtle text-xl font-semibold">Hi there! I'm</p>
             <h1 className="text-c-hero text-4xl font-bold md:text-6xl">Casper Pilgaard</h1>    
             <p className="text-c-subtle text-lg font-semibold md:text-2xl ">
-                I'm a Software Developer
+                Software Developer
             </p>
 
-            <p className="text-c-body font-normal mt-4 text-base max-w-sm"></p>
+            <p className="text-c-body font-normal mt-4 text-base max-w-sm "></p>
 
-            <Button className="border-black">Github</Button>
+            <a 
+                href="https://github.com/casperpilgaard" 
+                target="_blank" 
+                rel="noopener noreferrer"
+                className="flex items-center gap-3 border border-c-muted px-2 py-1 rounded-xl hover:border-c-accent transition-colors w-50"
+                >
+                <span className="text-c-accent">Check out my GitHub!</span>
+                <FaGithub className="text-7xl text-c-accent" />
+            </a>
         </div>
     )
 }
